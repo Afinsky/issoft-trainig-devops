3c796676dba9d7a637d0a90132f9b148d008ad11
Alexey Botyan <alexey.botyan@gmail.com>
2019-09-30 18:11:15 +0300

    Ne rabotaet

--- task2/bootstrap/bootstrap_centos.sh
+++ task2/bootstrap/bootstrap_centos.sh
@@ -2,12 +2,21 @@
 
 
 #sudo yum update -yqq
-sudo yum install -yqq git
 
-git clone git@github.com:Afinsky/training-devops.git
-git checkout module2
-echo "---------------------------------------------------------------------"
-cat training-devops/README.md
-echo "---------------------------------------------------------------------"
+sudo yum install -y -q git
 
+
+# whoami
+# sudo -i -u vagrant bash << EOF
+# echo "In"
+# whoami
+# 
+# ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+# ssh -T git@github.com
+# git clone git@github.com:Afinsky/training-devops.git
+# 
+# EOF
+# echo "Out"
+# whoami
+# 
 echo "CentOS done!"
