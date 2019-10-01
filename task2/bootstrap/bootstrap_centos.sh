3ddc0b2ae45de12adfa9c6169f9d196a4b8b8eae
Alexey Botyan <alexey.botyan@gmail.com>
2019-10-01 18:36:07 +0300

    Chota zarabotala

--- task2/bootstrap/bootstrap_centos.sh
+++ task2/bootstrap/bootstrap_centos.sh
@@ -6,13 +6,38 @@
 whoami
 sudo -i -u vagrant bash << EOF
 
+
+whoami
 sudo yum install -y -q git
 
-cat /tmp/authorized_keys >> ~/.ssh/authorized_keys
+#cat /tmp/authorized_keys >> ~/.ssh/authorized_keys
+
+
+chmod 644 ~/.ssh/id_rsa.pub
+chmod 600 ~/.ssh/id_rsa
 
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 
+#FUCK!!!
+#ssh -T git@github.com 
+
+echo "-------------------------------------"
+cd ~
+pwd
+echo "-------------------------------------"
+
+
+sudo runuser -l vagrant -c 'git clone git@github.com:Afinsky/trash-experience-etc.git'
+
+while [ ! -d ~/training-devops ] 
+do 
+
 git clone git@github.com:Afinsky/training-devops.git 
+
+done
+
+echo "-------------------------------------"
+
 
 echo "In"
 whoami
