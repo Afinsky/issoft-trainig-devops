f891efbb67a85d2e0e4cc2b480e0b75e26484429
Alexey Botyan <alexey.botyan@gmail.com>
2019-10-09 15:43:11 +0300

    Create logical volume after launch and mount its

--- task1/bootstrap/lvm.sh
+++ task1/bootstrap/lvm.sh
@@ -11,6 +11,16 @@ echo item: $i
 sudo pvcreate /dev/$i -f
 done
 
-cd /dev
-sudo vgcreate $vggrp $volumes
-cd -
+cd /dev && sudo vgcreate $vggrp $volumes && cd -
+
+sudo lvcreate -l 40%VG $vggrp
+
+dirs=`ls /dev/$vggrp`
+
+for j in $dirs
+do
+
+sudo mkfs.ext4 -F /dev/$vggrp/$j
+mkdir /mnt/$j
+mount /dev/$vggrp/$j /mnt/$j
+done
