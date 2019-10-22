319ed2e028a2f8bc8dc7617930a42442358379a8
Alexey Botyan <a.afinsky@gmail.com>
2019-10-22 09:03:35 +0300

    In process

---
+++ task1/bootstrap/scan_ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+list_for_scan=$(grep -v `hostname` /etc/hosts | grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" | grep -v '127.*')
+
+
+for i in $list_for_scan
+do
+
+echo item: $i
+ssh-keyscan -H $i >> ~/.ssh/known_hosts
+
+done
